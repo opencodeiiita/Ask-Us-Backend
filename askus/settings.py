f9e87054f78a3bc9f5200deb6382f619c9a7e981
Sagnik Mandal <acriticalcynic@outlook.com>
2021-10-06 00:06:58 +0530

    Added DRF to Installed Apps

--- askus/settings.py
+++ askus/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig', #This object was created for us in /users/apps.py
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
