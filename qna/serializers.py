5c7ed033e7db8765a0811f63326e42ca34936ce3
batflarrow <dwivedia2001@gmail.com>
2021-10-06 19:46:19 +0530

    Add question serializer

---
+++ qna/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Question
+
+
+class QuestionSerializer(ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
