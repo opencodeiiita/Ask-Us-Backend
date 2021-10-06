35d84369e1950d7789ae3ff88d7a2df840f275fc
adtoria <adityyyyadav@gmal.com>
2021-10-06 09:21:39 +0530

    Added Question model

--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=42)
+    description = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    no_of_answers = models.IntegerField(default=0)
+    no_of_upvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "\n".join([
+            f"[{title}]: {date_posted}",
+            f"{description}",
+            f"answers: {no_of_answers}",
+            f"upvotes: {no_of_upvotes}",
+        ])
