9b7a04ef4c2f760c02a0d2ab29aa4c7f36d35350
Sagnik Mandal <acriticalcynic@outlook.com>
2021-10-06 15:41:47 +0530

    Added Answer Model

--- qna/admin.py
+++ qna/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import (Question, Answer)
 
 admin.site.register(Question)
+admin.site.register(Answer)
 
 # Register your models here.
--- qna/models.py
+++ qna/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
@@ -14,4 +15,19 @@ class Question(models.Model):
             f"{description}",
             f"answers: {no_of_answers}",
             f"upvotes: {no_of_upvotes}",
+        ])
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    answer_text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    no_of_upvotes = models.IntegerField(default=0)
+    no_of_downvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "\n".join([
+            f"{answer_text}",
+            f"upvotes: {no_of_upvotes}",
+            f"downvotes: {no_of_downvotes}"
         ])
