fc2f7ee4116cd0bb6690116840e7fa58cdffce4e
Monik09 <monikinderjitsingh5@gmail.com>
2021-10-06 15:36:33 +0530

    used built in user model

--- users/models.py
+++ users/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 from sorl.thumbnail import get_thumbnail
 
 # Create your models here.
 
 class UserModel(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     questionsAsked = models.IntegerField(default=0)
     answersGiven = models.IntegerField(default=0)
     profilePic =  models.ImageField(upload_to='...')
 
     def __str__(self):
-        return "\n".join([f"profilePic:{profilePic}", f"questionsAsked:{questionsAsked}", f"answersGiven:{answersGiven}"])
+        return "\n".join([f"profilePic:{self.profilePic}", f"questionsAsked:{self.questionsAsked}", f"answersGiven:{self.answersGiven}"])
     
     def save(self, *args, **kwargs):
         if self.profilePic:
-            self.profilePic = get_thumbnail(self.profilePic, '300x300', quality=99, format='JPEG')
+            self.profilePic = get_thumbnail(self.profilePic, '300x300', quality=99, format='JPEG').url
         super(UserModel, self).save(*args, **kwargs)
