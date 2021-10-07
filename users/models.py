bd2820404056fef3d638fa2e5d1f527f304aec57
Sagnik Mandal <acriticalcynic@outlook.com>
2021-10-07 12:13:37 +0530

    Add media directory

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class UserModel(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     questionsAsked = models.IntegerField(default=0)
     answersGiven = models.IntegerField(default=0)
-    profilePic =  models.ImageField(upload_to='profile_pics', default='default.png')
+    profilePic =  models.ImageField(upload_to='media/profile_pics', default='default.png')
 
     def __str__(self):
         return "\n".join([f"profilePic:{self.profilePic}", f"questionsAsked:{self.questionsAsked}", f"answersGiven:{self.answersGiven}"])
