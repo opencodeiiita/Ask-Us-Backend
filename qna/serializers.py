7d65886a088ccc6a6c3d87c083bd1a2ae431e1d8
diyawaghmare <diya.waghmare@gmail.com>
2021-10-08 22:17:20 +0530

    Created serializer for Answer

--- qna/serializers.py
+++ qna/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Question
+from .models import Answer, Question
 
 
 class QuestionSerializer(ModelSerializer):
     class Meta:
         model = Question
+        fields = "__all__"
+
+class AnswerSerializer(ModelSerializer):
+    class Meta:
+        model = Answer
         fields = "__all__"
