11569b6bc94d28b83658b00a90f7c4229776c077
pankaj1707k <pankaj.1707k@gmail.com>
2021-10-09 21:27:47 +0530

    Some clean up

--- qna/views.py
+++ qna/views.py
@@ -2,13 +2,10 @@
 # Create your views here.
 from rest_framework import status
 from rest_framework.decorators import api_view
-
 from rest_framework.response import Response
+from qna.models import Question, Answer
+from qna.serializers import QuestionSerializer, AnswerSerializer
 
-from qna.models import Question
-from qna.models import Answer
-from qna.serializers import QuestionSerializer
-from qna.serializers import AnswerSerializer
 
 @api_view(['GET', 'POST'])
 def question_list(request):
@@ -23,7 +20,8 @@ def question_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def question_detail(request, **kwargs):
 
@@ -46,6 +44,7 @@ def question_detail(request, **kwargs):
     elif request.method == "DELETE":
         question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['GET', 'POST'])
 def answer_list(request):
