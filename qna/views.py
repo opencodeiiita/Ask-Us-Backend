72ba1f09185b09e6686868b1e88132a7ffea4c2d
jalanprakhar <75791114+jalanprakhar@users.noreply.github.com>
2021-10-09 20:12:08 +0530

    Added a function-based api view for the Answer model that handles GET and POST (#31)
    * Added a function-based api view for the Answer model that handles GET and POST requests.
    
    * Update views.py

--- qna/views.py
+++ qna/views.py
@@ -6,7 +6,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from qna.models import Question
+from qna.models import Answer
 from qna.serializers import QuestionSerializer
+from qna.serializers import AnswerSerializer
 
 @api_view(['GET', 'POST'])
 def question_list(request):
@@ -24,6 +26,7 @@ def question_list(request):
         
 @api_view(['GET', 'PUT', 'DELETE'])
 def question_detail(request, **kwargs):
+
     _id = kwargs.get("id")
     try:
         question = Question.objects.get(id=_id)
@@ -43,3 +46,22 @@ def question_detail(request, **kwargs):
     elif request.method == "DELETE":
         question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def answer_detail(request, **kwargs):
+     _id = kwargs.get("id")
+    try:
+        answer = Answer.objects.get(id=_id)
+    except Answer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        data = AnswerSerializer(answer).data
+        return Response(data)
+    elif request.method == 'POST':
+        serializer = AnswerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
