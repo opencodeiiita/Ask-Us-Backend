dc1c04db7bc8258ac3d2628a367f1a4b46991a24
jalanprakhar <iit2020200@iiita.ac.in>
2021-10-12 19:10:11 +0530

    added user serializer

--- qna/serializers.py
+++ qna/serializers.py
@@ -13,3 +13,8 @@ class AnswerSerializer(ModelSerializer):
         model = Answer
         fields = "__all__"
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name")
+
