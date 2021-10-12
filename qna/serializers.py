dd1abab1bf7e6b1217408ab8fc172fcd88026cb4
jalanprakhar <iit2020200@iiita.ac.in>
2021-10-12 19:11:50 +0530

    mistake recified

--- qna/serializers.py
+++ qna/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Answer, Question
+from .models import Answer, Question,User
 
 
 class QuestionSerializer(ModelSerializer):
