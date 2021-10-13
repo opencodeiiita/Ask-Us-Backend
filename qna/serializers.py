f66f325d276b969a60f70a4d5381cc3800947241
rushilpatra <70966802+rushilpatra@users.noreply.github.com>
2021-10-13 16:03:05 +0530

    implemented the validate and create method

--- qna/serializers.py
+++ qna/serializers.py
@@ -13,3 +13,29 @@ class AnswerSerializer(ModelSerializer):
         model = Answer
         fields = "__all__"
 
+class RegisterSerializer(ModelSerializer):
+    email = ModelSerializer.CharField(required = True)
+    password = ModelSerializer.CharField(required = True,Write_only = True)
+    password2 = ModelSerializer.CharField(required = True,Write_only = True)
+
+        class Meta:
+            model = User
+            fields = ['id','username','password','password2','email','first_name','last_name']
+
+def create(self,validated_data):
+     user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+return user
+
+   
+
+def validate(self,validated_data):
+    password = self.validated_data['password']
+    password2 = self.validated_data['password2']
+    if password != password2:
+        raise serializers.ValidationError({'password':'passwords must match'})
+return password
+
+    
