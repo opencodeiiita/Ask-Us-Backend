8cf6dcad6776b85602669fdeaff31403a1eaa6b3
ksathwik03 <iit2020241@iiita.ac.in>
2021-10-14 13:26:30 +0530

    final commit

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from users import views
 
 urlpatterns = [
-    path('register/', views.UserRegisterView),
+    path('register/', UserRegisterView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(['POST'])
-def UserRegisterView():
-    print('userregister')
