d2a8cec24b93c8edf9501ab0c50bfc43ba50f935
ksathwik03 <iit2020241@iiita.ac.in>
2021-10-14 12:58:11 +0530

    small bug

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 from users import views
 
 urlpatterns = [
-    path('/register/', views.UserRegisterView),
-    
+    path('register/', views.UserRegisterView),
 ]
