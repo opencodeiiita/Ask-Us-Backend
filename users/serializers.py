adf13fc64ad13147770efa9132ad798b85a576a8
Agrim Verma <agrimav000@gmail.com>
2021-10-15 17:31:28 +0530

    updated RegisterSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,9 @@
+from typing_extensions import Required
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -7,13 +11,14 @@ class UserSerializer(ModelSerializer):
         fields = ("id", "username", "email", "first_name", "last_name")
 
 class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(required = True)
+    email = serializers.EmailField(required = True)
     password = serializers.CharField(required = True,write_only = True)
     password2 = serializers.CharField(required = True,write_only = True)
 
     class Meta:
         model = User
         fields = ['id','username','password','password2','email','first_name','last_name']
+
 
     def create(self,validated_data):
         user = User.objects.create_user(
@@ -23,11 +28,15 @@ class RegisterSerializer(serializers.ModelSerializer):
             )
         return user
 
-       
-
     def validate(self,data):
         password = data['password']
         password2 = data['password2']
         if password != password2:
             raise serializers.ValidationError({'password':'passwords must match'})
         return data
+
+    def validate_email(self,data):
+        email_lower = data['email'].lower()
+        if User.objects.filter(email__iexact=email_lower).exists():
+            raise serializers.ValidationError({'email':'Email already exists'})
+        return email_lower
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
