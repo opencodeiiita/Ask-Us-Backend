c7d326cc43581112b70197d6738c8f600b477da9
Agrim Verma <91134684+AgrimVerma@users.noreply.github.com>
2021-10-15 19:00:00 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-from .serializers import RegisterSerializer
-from django.contrib.auth.models import User
-from rest_framework import generics
-
-class UserRegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
