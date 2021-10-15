b0bc676648124bb7d7e865f8f81aca221662a467
diyawaghmare <56413380+diyawaghmare@users.noreply.github.com>
2021-10-15 21:18:34 +0530

    API view for user registration (#67)

--- users/views.py
+++ users/views.py
@@ -1,1 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+
+from users.serializers import RegisterSerializer
+
+class UserRegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = User.objects.all()
+
