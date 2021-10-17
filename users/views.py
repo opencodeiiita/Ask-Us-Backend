204ebfdf7d900fb61d95cd44e2256c3ff2b1fc60
raghavgoel25 <75788719+raghavgoel25@users.noreply.github.com>
2021-10-17 13:27:04 +0530

    Delete views.py

--- users/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.generics import CreateAPIView
-from django.contrib.auth.models import User
-
-from users.serializers import RegisterSerializer
-
-class UserRegisterView(CreateAPIView):
-    serializer_class = RegisterSerializer
-    queryset = User.objects.all()
-
