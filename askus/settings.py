0fbb2b5df8005c690bd3b623be33240707aa6040
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2021-10-17 11:22:25 +0530

    Update settings.py

--- askus/settings.py
+++ askus/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-        'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
