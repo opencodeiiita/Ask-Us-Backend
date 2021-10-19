899b70767f17f3d63d29217e2bccb494e26e2a8e
Ksathwik03 <76401429+Ksathwik03@users.noreply.github.com>
2021-10-19 02:38:46 -0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth.models import User
-
+from rest_framework.permissions import AllowAny
 from users.serializers import RegisterSerializer
 
 class UserRegisterView(CreateAPIView):
