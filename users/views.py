c9619def4a5c4a156edf471c7e6ff750a9afb6a9
diyawaghmare <diya.waghmare@gmail.com>
2021-10-22 22:37:18 +0530

    Fixed APIView

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import CreateAPIView
-from rest_framework.generics import APIView
+from rest_framework.views import APIView
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
