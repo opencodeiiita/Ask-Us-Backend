82a104af2b351888aed8b48855610011ae8ad89e
Ksathwik03 <76401429+Ksathwik03@users.noreply.github.com>
2021-10-25 03:28:17 -0500

    View for changing password (#100)
    * change password
    
    * small changes
    
    * small bug
    
    * wrong view

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from users.serializers import RegisterSerializer,UserSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import UpdateAPIView
 
 class UserRegisterView(CreateAPIView):
     permission_classes = [AllowAny]
@@ -31,3 +32,23 @@ class ListUsers(APIView):
     def get(self, request, format=None):
          user=UserSerializer(User.objects.all().filter(is_staff=False),many=True)
          return Response(user.data,status=status.HTTP_200_OK)
+
+class ChangePasswordView(UpdateAPIView):
+        serializer_class = ChangePasswordSerializer
+        model = User
+        def update(self, request, *args, **kwargs):
+            self.object = self.request.user
+            serializer = self.get_serializer(data=request.data)
+
+            if serializer.is_valid():
+                if not self.object.check_password(serializer.data.get("old_password")):
+                    return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+                self.object.set_password(serializer.data.get("new_password"))
+                self.object.save()
+                response = {
+                    'message': 'Password updated successfully',
+                }
+
+                return Response(status= status.HTTP_200_OK, data = response)
+
+            return Response( status=status.HTTP_400_BAD_REQUEST)
