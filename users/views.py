a75d6f796a38d5f8ab7746d1d40b6cba8b0180f5
Ksathwik03 <iit2020241@iiita.ac.in>
2021-10-25 13:08:54 +0530

    change password

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import CreateAPIView
-from rest_framework.views import APIView
+from rest_framework.views import APIView,UpdateAPIView
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
@@ -31,3 +31,24 @@ class ListUsers(APIView):
     def get(self, request, format=None):
          user=UserSerializer(User.objects.all().filter(is_staff=False),many=True)
          return Response(user.data,status=status.HTTP_200_OK)
+
+class ChangePasswordView(UpdateAPIView):
+        serializer_class = ChangePasswordSerializer
+        model = User
+        def update(self, request, *args, **kwargs):
+            self.object = self.request.user
+            serializer = self.get_serializer(data=request.data)
+
+            if serializer.is_valid():
+                if not self.object.check_password(serializer.data.get("old_password")):
+                    return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+                self.object.set_password(serializer.data.get("new_password"))
+                self.object.save()
+                response = {
+                    'status': status.HTTP_200_OK,
+                    'message': 'Password updated successfully',
+                }
+
+                return Response(response)
+
+            return Response( status=status.HTTP_400_BAD_REQUEST)
