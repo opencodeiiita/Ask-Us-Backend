a47704a9ca7fb4aa7b3ffa3321abbb1e78d8eba8
Ksathwik03 <iit2020241@iiita.ac.in>
2021-10-25 13:14:58 +0530

    small changes

--- users/views.py
+++ users/views.py
@@ -44,11 +44,7 @@ class ChangePasswordView(UpdateAPIView):
                     return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
                 self.object.set_password(serializer.data.get("new_password"))
                 self.object.save()
-                response = {
-                    'status': status.HTTP_200_OK,
-                    'message': 'Password updated successfully',
-                }
 
-                return Response(response)
+                return Response(status= status.HTTP_200_OK, message= 'Password updated successfully')
 
             return Response( status=status.HTTP_400_BAD_REQUEST)
