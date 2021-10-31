b244aa580677cfee7737d5404f546d4413df575c
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2021-10-31 10:58:57 +0530

    Changed page size

--- askus/settings.py
+++ askus/settings.py
@@ -112,7 +112,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':1,
+    'PAGE_SIZE':10,
 }
 
 # Internationalization
