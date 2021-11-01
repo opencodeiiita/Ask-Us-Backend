f7a867a73772bd9f3b547eae84c2d8df436ac3bd
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2021-11-01 23:48:13 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('<str:username>/questions/', views.ListQuestionsByUser.as_view()),
     path('<str:username>/answers/', views.ListAnswersByUser.as_view()),
     path('<str:username>/update/', views.UserUpdateView.as_view()),
-    path('<str:username>/profile_pic/', views.UploadProfilePic.as_view()) 
+    path('<str:username>/profile_pic/upload/', views.UploadProfilePic.as_view()) 
 ]    
